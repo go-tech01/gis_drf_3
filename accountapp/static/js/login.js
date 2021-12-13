ad5adb3201842448107583d7e67e9c1b7d51cc8b
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-13 15:34:40 +0900

    login redirect

--- accountapp/static/js/login.js
+++ accountapp/static/js/login.js
@@ -26,12 +26,10 @@ function send_input() {
         .then(function (response) {
             // 성공했을 경우
             console.log(response);
-            document.getElementById('alert_box').innerHTML
-                = "<div class='btn btn-primary rounded-pill px-5'>로그인이 성공했습니다</div>";
-
             // Token 수령 후 쿠키 생성
             setCookie('drf_token', 'Token ' + response.data['token']);
-
+            // suceees_url
+            window.location.href = '/accounts/hello_world_template/'
         })
         .catch(function (error) {
             // 실패했을 경우
