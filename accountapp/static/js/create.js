39fe71fec94ade18a8edb3b9d17ec108359d716a
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-13 10:43:51 +0900

    DRF account create javascript update 2

---
+++ accountapp/static/js/create.js
@@ -0,0 +1,16 @@
+function send_input() {
+    axios.post('/accounts/create/', {
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value,
+    })
+        .then(function (response) {
+            console.log(response);
+            document.getElementById('alert_box').innerHTML
+                = "<div class='btn btn-primary rounded-pill px-5'>가입이 성공했습니다</div>"
+        })
+        .catch(function (error) {
+            console.log(error);
+            document.getElementById('alert_box').innerHTML
+                = "<div class='btn btn-danger rounded-pill px-5'>가입이 실패했습니다</div>"
+        });
+}
