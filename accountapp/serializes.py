2fb13492a6c7ae0c7b2d0056024697a8530d80c7
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-13 13:22:33 +0900

    Postman list view

--- accountapp/serializes.py
+++ accountapp/serializes.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+
 class NewModelSerializer(serializers.Serializer):
     text = serializers.CharField(max_length=255)
     created_at = serializers.DateTimeField()
@@ -9,3 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username','password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
