65ebb924dff84ee4f21a4fcc35960cce1d69dcc9
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-13 14:36:07 +0900

    Account Login Template 1

--- accountapp/static/js/login.js
+++ accountapp/static/js/login.js
@@ -7,7 +7,8 @@ function send_input() {
             console.log(response);
             document.getElementById('alert_box').innerHTML
                 = "<div class='btn btn-primary rounded-pill px-5'>로그인이 성공했습니다</div>"
-            window.location.href = '/accounts/hello_world_template/'
+            document.cookie='drf2_token=Token'+response.data['token']
+//            window.location.href = '/accounts/hello_world_template/'
         })
         .catch(function (error) {
             console.log(error);
