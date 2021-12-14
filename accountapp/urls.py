60ba6fb558271e336added25b5278af033ae0cf1
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-14 10:26:36 +0900

    Update API View

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from rest_framework.authtoken import views
 
 from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView, \
-    AccountLoginTemplate, AccountRetrieveAPIView, AccountRetrieveTemplateView
+    AccountLoginTemplate, AccountRetrieveAPIView, AccountRetrieveTemplateView, AccountUpdateAPIView
 
 app_name = 'accountapp'
 
@@ -20,4 +20,6 @@ urlpatterns = [
 
     path('retrieve_template/<int:pk>', AccountRetrieveTemplateView.as_view(), name='retrieve_template'),
     path('retrieve/<int:pk>', AccountRetrieveAPIView.as_view(), name='retrieve'),
+
+    path('update/<int:pk>', AccountUpdateAPIView.as_view(), name='update'),
 ]
