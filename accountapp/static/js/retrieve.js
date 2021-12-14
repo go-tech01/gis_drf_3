6c7af6213629d355ca19dd4210594fe92bd81bb9
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-14 09:44:24 +0900

    RetrieveView Template 1

--- accountapp/static/js/retrieve.js
+++ accountapp/static/js/retrieve.js
@@ -1,12 +1,16 @@
-axios.get('/accounts/retrieve_template/{{ request.GET.get("pk") }}')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+function initialize(pk) {
+    axios.get('/accounts/retrieve/' + pk )
+        .then(function (response) {
+            // handle success
+            console.log(response);
+            document.getElementById('username').innerHTML = response.data['username'];
+            document.getElementById('date_joined').innerHTML = response.data['date_joined'];
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+}
