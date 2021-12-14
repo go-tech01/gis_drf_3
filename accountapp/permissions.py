2cc4041d725539e06c496f97f793ea7dd7c9e66c
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-14 16:50:45 +0900

    RUD & owner permission

--- accountapp/permissions.py
+++ accountapp/permissions.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj == request.user
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return obj == request.user
 
     # def has_permission(self, request, view):
     #     user = User.objects.get(pk=view.kwargs['pk'])
