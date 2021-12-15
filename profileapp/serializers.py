52e4e37b865fa4a7dbbcdc5c4eeb5f9a535925a8
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-15 16:02:23 +0900

    Profile Create Template javascript

--- profileapp/serializers.py
+++ profileapp/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework.serializers import ModelSerializer
-
 from profileapp.models import Profile
-
 
 class ProfileSerializer(ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['nickname','image','message']
+        fields = ['id','nickname','image','message']
---
+++ profileapp/static/profileapp/js/create.js
@@ -0,0 +1,25 @@
+function send_input() {
+    var form = new FormData();
+    form.append('nickname', document.getElementById('nickname').value);
+    form.append('message', document.getElementById('message').value);
+    form.append('image', document.getElementById('image').files[0]);
+    axios({
+        method:'post',
+        url:'/profiles/',
+        data: form,
+        headers:{
+            Authorization: decodeURIComponent(getCookie('drf_token'))
+        },
+    })
+        .then(function (response) {
+            console.log(response);
+//            window.location.href = 'accounts/retrieve/'+pk;
+            document.getElementById('alert_box').innerHTML
+                = "<div class='btn btn-primary rounded-pill px-5'>프로필 작성에 성공했습니다</div>"
+        })
+        .catch(function (error) {
+            console.log(error);
+            document.getElementById('alert_box').innerHTML
+                = "<div class='btn btn-danger rounded-pill px-5'>프로필 작성에 실패했습니다</div>"
+        });
+}
