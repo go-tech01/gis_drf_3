5ce937e8b54c99496fb88c1e50d119ebb1bf326b
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-16 11:10:19 +0900

    Profieleapp Template js pk & RetriUpdateview

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -4,7 +4,7 @@ from profileapp.views import ProfileCreateAPIView, ProfileCreateTemplateView, Pr
 
 urlpatterns = [
     path('create_template/', ProfileCreateTemplateView.as_view(), name='create_template'),
-    path('update_template/', ProfileUpdateTemplateView.as_view(), name='update_template'),
+    path('update_template/<int:pk>', ProfileUpdateTemplateView.as_view(), name='update_template'),
     path('', ProfileCreateAPIView.as_view(),name='create'),
     path('<int:pk>', ProfileRetrieveUpdateAPIView.as_view(), name='update'),
 ]
