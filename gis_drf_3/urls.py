d2cb9961468a178402eb5d049c6fa799db8f6ce2
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-17 09:20:38 +0900

    Article App Create

--- gis_drf_3/urls.py
+++ gis_drf_3/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls')),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
 + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
