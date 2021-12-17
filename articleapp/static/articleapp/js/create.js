4e8e44668050037583f90bc69f1cc593b176a3c1
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-17 10:37:27 +0900

    Article App Create Template

---
+++ articleapp/static/articleapp/js/create.js
@@ -0,0 +1,25 @@
+function send_input() {
+    var form = new FormData();
+    form.append('title', document.getElementById('title').value);
+    form.append('content', document.getElementById('content').value);
+    form.append('image', document.getElementById('image').files[0]);
+    axios({
+        method:'post',
+        url:'/articles/',
+        data: form,
+        headers:{
+            Authorization: decodeURIComponent(getCookie('drf_token'))
+        },
+    })
+        .then(function (response) {
+            console.log(response);
+            document.getElementById('alert_box').innerHTML
+                = "<div class='btn btn-primary rounded-pill px-5'>게시글 작성에 성공했습니다</div>"
+//            window.location.href = '/articles/retrieve_template/'+response.data['id'];
+        })
+        .catch(function (error) {
+            console.log(error);
+            document.getElementById('alert_box').innerHTML
+                = "<div class='btn btn-danger rounded-pill px-5'>게시글 작성에 실패했습니다</div>"
+        });
+}
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from articleapp.models import Article
 from articleapp.serializers import ArticleSerializer
+
+class ArticleCreateTemplateView(TemplateView):
+    template_name = 'articleapp/create.html'
 
 class ArticleCreateAPIView(CreateAPIView):
     queryset = Article
